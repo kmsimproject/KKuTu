544ce4cc3ac3222d7f6f5238899ac2445c0a86ce
metroracer <68764872+kmsimproject@users.noreply.github.com>
2021-05-12 10:36:52 +0900

    클플적용

--- Server/lib/Game/cluster.js
+++ Server/lib/Game/cluster.js
@@ -21,6 +21,8 @@ var Const = require('../const');
 var JLog = require('../sub/jjlog');
 var SID = Number(process.argv[2]);
 var CPU = Number(process.argv[3]); //require("os").cpus().length;
+var Global = require('../sub/global.json');
+var CF = Global.USE_CLOUDFLARE;
 
 if(isNaN(SID)){
 	if(process.argv[2] == "test"){
@@ -35,13 +37,16 @@ if(isNaN(CPU)){
 	console.log(`Invalid CPU Number ${process.argv[3]}`);
 	process.exit(1);
 }
+if(CPU >= 2 && CF){
+	console.log(`Max Channel Limit is 1 when CloudFlare is Enabled.`);
+}
 if(Cluster.isMaster){
 	var channels = {}, chan;
 	var i;
 	
 	for(i=0; i<CPU; i++){
 		chan = i + 1;
-		channels[chan] = Cluster.fork({ SERVER_NO_FORK: true, KKUTU_PORT: Const.MAIN_PORTS[SID] + 416 + i, CHANNEL: chan });
+		channels[chan] = Cluster.fork({ SERVER_NO_FORK: true, KKUTU_PORT: Const.MAIN_PORTS[SID] + (CF ? 30 : 416) + i, CHANNEL: chan });
 	}
 	Cluster.on('exit', function(w){
 		for(i in channels){
@@ -51,7 +56,7 @@ if(Cluster.isMaster){
 			}
 		}
 		JLog.error(`Worker @${chan} ${w.process.pid} died`);
-		channels[chan] = Cluster.fork({ SERVER_NO_FORK: true, KKUTU_PORT: Const.MAIN_PORTS[SID] + 416 + (chan - 1), CHANNEL: chan });
+		channels[chan] = Cluster.fork({ SERVER_NO_FORK: true, KKUTU_PORT: Const.MAIN_PORTS[SID] + (CF ? 30 : 416) + (chan - 1), CHANNEL: chan });
 	});
 	process.env['KKUTU_PORT'] = Const.MAIN_PORTS[SID];
 	require("./master.js").init(SID.toString(), channels);
